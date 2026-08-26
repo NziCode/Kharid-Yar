673596bbb4c0ec25b4203ef3907cf71761c817cc
Mohammad Ali Nazari <nazari.moradkhani@gmail.com>
2026-08-26 13:22:05 +0330

    Set Persian app name Kharidyar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.kharidyar.app',
-  appName: 'Kharid Yar',
+  appName: 'خریدیار',
   webDir: 'dist',
   bundledWebRuntime: false
 };
